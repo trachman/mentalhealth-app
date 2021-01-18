e2ef9a761b1f9b90a07872b54cf7cfd6b061ef9b
trachman <tsrachman7@gmail.com>
2021-01-18 18:40:22 -0500

    banner animation

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,6 +5,10 @@ const Title = () => {
 		<>
 			<div className="TitleWrapper">
 				<a href="https://boltbootcamps.com/" target="_blank" rel="noreferrer"><img src={logo} alt="BOLT Logo" className="Title"/></a>
+				<div className="Description-Wrapper">
+					<p className="message">A simple CRUD App to get you started!</p>
+					<div id="DescriptionSlider"></div>
+				</div>
 			</div>
 		</>
 	);
